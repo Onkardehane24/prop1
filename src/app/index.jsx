8e0f6ccff9070aa526c9b1b4de3f0e92636e8112
onkardehane <onkar.dehane24@gmail.com>
2020-04-23 02:23:33 +0200

    working on the app

---
+++ src/app/index.jsx
@@ -0,0 +1,12 @@
+//console.log("hellow world")
+
+import { store } from './store'
+import React from 'react';
+import ReactDOM from 'react-dom'
+import {Main} from './components/Main'
+//console.log(store.getState());
+
+ReactDOM.render(
+    <Main/>,
+    document.getElementById("app")
+);
